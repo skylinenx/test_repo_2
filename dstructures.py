36e3171d57f9ec621fe7d35a01899b503fecb534
skylinenx <nisalsahan@gmail.com>
2024-04-04 20:10:54 +0530

    progressing python

---
+++ dstructures.py
@@ -0,0 +1,29 @@
+#string
+name = "Nisal"
+print(name[0]) #N
+
+#list
+names = ["Nisal", "Sandy", "Sky"]
+print(names)    #['Nisal', 'Sandy', 'Sky']
+print(names[0])     #Nisal
+print(names[1][0])  #S
+
+names.append("Mittens") #added to the end of list
+names.remove("Sky")
+names.sort() #sort alphebetically
+print(names) #['Mittens', 'Nisal', 'Sandy']
+print(f"items in names list: {len(names)}")
+
+#tuple
+cordinates = (20.5, -56.3)
+print(cordinates)   #(20.5, -56.3)
+print(cordinates[0])    #20.5
+
+#set
+s = {10,20}
+s.add(10)
+s.add(30)
+s.remove(30)
+print(s)    #{10, 20}
+
+#dict
